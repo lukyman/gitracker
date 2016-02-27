bf9886de620cc3823a5a41238cbfc7022f4ec5e6
abdulmanan <lukyman>
2016-02-27 09:54:04 +0300

    inital commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <!-- <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css"> -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h1>helo</h1>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/include.bootstrap.blade.php
@@ -0,0 +1,5 @@
+@extends('views.home')
+{!!Html::script('js/bootstrap.min.js')!!}
+
+{!!Html::style('css/bootstrap.min.css')}
+
